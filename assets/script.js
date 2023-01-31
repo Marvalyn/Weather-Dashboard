2aebd14864697f750891c45571260f809e2a853e
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-01-31 21:25:32 +0000

    icons won't generate now

--- assets/script.js
+++ assets/script.js
@@ -74,23 +74,23 @@ function displayWeatherInfo(city) {
         //icons
         var iconOne = $("#icon-1");
         var forecastIconOne = response.list[1].weather[0].icon;
-        iconOne.attr("src", "https://openweathermap.org/img/wn/" + forecastIconOne + "@2x.png");
+        iconOne.attr("src", "http://openweathermap.org/img/wn/" + forecastIconOne + "@2x.png");
 
         var iconTwo = $("#icon-2");
         var forecastIconTwo = response.list[2].weather[0].icon;
-        iconTwo.attr("src", "https://openweathermap.org/img/wn/" + forecastIconTwo + "@2x.png");
+        iconTwo.attr("src", "http://openweathermap.org/img/wn/" + forecastIconTwo + "@2x.png");
 
         var iconThree = $("#icon-3");
         var forecastIconThree = response.list[3].weather[0].icon;
-        iconThree.attr("src", "https://openweathermap.org/img/wn/" + forecastIconThree + "@2x.png");
+        iconThree.attr("src", "http://openweathermap.org/img/wn/" + forecastIconThree + "@2x.png");
 
         var iconFour = $("#icon-4");
         var forecastIconFour = response.list[4].weather[0].icon;
-        iconFour.attr("src", "https://openweathermap.org/img/wn/" + forecastIconFour + "@2x.png");
+        iconFour.attr("src", "http://openweathermap.org/img/wn/" + forecastIconFour + "@2x.png");
 
         var iconFive = $("#icon-5");
         var forecastIconFive = response.list[5].weather[0].icon;
-        iconFive.attr("src", "https://openweathermap.org/img/wn/" + forecastIconFive + "@2x.png");
+        iconFive.attr("src", "http://openweathermap.org/img/wn/" + forecastIconFive + "@2x.png");
 
         //temperature
         var tempOne = $("#temperature1");
